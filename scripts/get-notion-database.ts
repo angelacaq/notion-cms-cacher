d9bce513fde4a9940e04ad86e36dd5db15fb5b47
Angela Chen <angela@Angelas-MacBook-Air.local>
2023-01-30 17:37:29 -0800

    Retrieve database from Notion and log

--- scripts/get-notion-database.ts
+++ scripts/get-notion-database.ts
@@ -1,4 +1,17 @@
+import * as dotenv from "dotenv"
+
+const { Client } = require("@notionhq/client")
+
+dotenv.config()
+
 async function main() {
-    console.log("hi world")
+    const notionClient = new Client({ auth: process.env.NOTION_TOKEN})
+    const notionUsers = await notionClient.users.list()
+    console.log(notionUsers)
+
+    const databaseResponse = await notionClient.databases.retrieve({
+        database_id: process.env.NOTION_DATABASE_ID
+    })
+    console.log(databaseResponse)
 }
 main()
